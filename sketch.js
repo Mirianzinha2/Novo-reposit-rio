c7dbc9332a69a87b84657612d48c60626d69031a
Mirianzinha2 <mhiisanthos2@gmail.com>
2024-05-22 15:06:53 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(400, 400);
+
+  }
+  
+
+  function draw() {
+
+    stroke ("blue");
+    fill ("red");
+
+
+  // console.log(mouseIsPressed);
+  if (mouseIsPressed) {
+    
+  Reflect(mouseIsPressed){
+
+    background(220);
+  }
